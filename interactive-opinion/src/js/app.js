23c894015544646d01d9f250a90172c39adb373c
Kate Whalen <whalenka88@gmail.com>
2016-12-19 16:33:59 +0000

    Enable  emailvariants in app webview

--- interactive-opinion/src/js/app.js
+++ interactive-opinion/src/js/app.js
@@ -1,25 +1,60 @@
+(function () {
 
-function isWebView() {
-  return (typeof window.GU !== 'undefined') ? true : false;
-}
+  var VARIANTS = [
+      {
+          id: 'Opinion-UK-Control',
+          test: function () {
+              const emailListID = '9999';
+              const exampleUrl = '';
+              updateWebView(emailListID);
+              updateExampleUrl(exampleUrl);
+          }
+      },
+      {
+          id: 'Opinion-UK-Variant',
+          test: function () {
+              const emailListID = '8888';
+              const exampleUrl = '';
+              updateWebView(emailListID);
+              updateExampleUrl(exampleUrl);
+          }
+      }
+  ];
 
-function getRandomBucket(list) {
-  return list[Math.floor((
-    Math.random() * list.length
-  ))];
-}
+  function isWebView() {
+    // window.GU (case is important!) only exists in the app.
+    // Therefore, window.GU can be used to detect when an article is running in an App WebView
+    return (typeof window.GU !== 'undefined') ? true : false;
+  }
 
-function updateWebView(emailListID) {
-    var emailForm = document.getElementsByClassName('js-email-sub__iframe')[0];
-    emailForm.setAttribute('src', 'https://www.theguardian.com/email/form/plaintone/' + emailListID);
-}
+  function getRandomBucket(variants) {
+    return variants[Math.floor(
+      Math.random() * variants.length
+    )];
+  }
 
-// init takes a list of strings, that correspond to the listIDs of all the variants in the email test
-function init(listIDs) {
-    var listID = getRandomBucket(listIDs);
-    updateWebView(listID);
-}
+  function updateWebView(emailListID) {
+      const emailForm = document.getElementsByClassName('js-email-sub__iframe')[0];
+      const formUrl = 'https://www.theguardian.com/email/form/plaintone/' + emailListID
+      if (emailForm) {
+        emailForm.setAttribute('src', formUrl);
+      }
+  }
 
-if (isWebView()) {
-  init(['9999', '8888']);
-}
+  function updateExampleUrl(exampleUrl) {
+    const example = document.getElementsByClassName('.js-email-example')[0];
+    if(example) {
+      example.setAttribute('href', exampleUrl);
+    }
+  }
+
+  function init(variants) {
+      const variant = getRandomBucket(variants);
+      variant.test();
+  }
+
+  if (isWebView()) {
+    init(VARIANTS);
+  }
+
+}());
