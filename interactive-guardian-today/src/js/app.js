d7916b8be4b7db283e4b18e77b40d52ef9b6ddb6
Kate Whalen <whalenka88@gmail.com>
2016-12-19 17:05:32 +0000

    Add basic Guardian Today email sign up interactive

---
+++ interactive-guardian-today/src/js/app.js
@@ -0,0 +1,60 @@
+(function () {
+
+  var VARIANTS = [
+      {
+          id: 'Guardian-Today-UK-Control',
+          test: function () {
+              const emailListID = '9999';
+              const exampleUrl = '';
+              updateWebView(emailListID);
+              updateExampleUrl(exampleUrl);
+          }
+      },
+      {
+          id: 'Guardian-Today-UK-Variant',
+          test: function () {
+              const emailListID = '8888';
+              const exampleUrl = '';
+              updateWebView(emailListID);
+              updateExampleUrl(exampleUrl);
+          }
+      }
+  ];
+
+  function isWebView() {
+    // window.GU (case is important!) only exists in the app.
+    // Therefore, window.GU can be used to detect when an article is running in an App WebView
+    return (typeof window.GU !== 'undefined') ? true : false;
+  }
+
+  function getRandomBucket(variants) {
+    return variants[Math.floor(
+      Math.random() * variants.length
+    )];
+  }
+
+  function updateWebView(emailListID) {
+      const emailForm = document.getElementsByClassName('js-email-sub__iframe')[0];
+      const formUrl = 'https://www.theguardian.com/email/form/plaintone/' + emailListID
+      if (emailForm) {
+        emailForm.setAttribute('src', formUrl);
+      }
+  }
+
+  function updateExampleUrl(exampleUrl) {
+    const example = document.getElementsByClassName('.js-email-example')[0];
+    if(example) {
+      example.setAttribute('href', exampleUrl);
+    }
+  }
+
+  function init(variants) {
+      const variant = getRandomBucket(variants);
+      variant.test();
+  }
+
+  if (isWebView()) {
+    init(VARIANTS);
+  }
+
+}());
