003927952ab6c5a54d0a567d7c428975bc21f61a
David Furey <david.furey@guardian.co.uk>
2017-01-12 14:11:12 +0000

    Changes for A/B test
    - Update ids
    - remove example url (legacy version doesn’t have a preview)
    - Set legacy to get 10% of signups in apps

--- interactive-opinion/src/js/app.js
+++ interactive-opinion/src/js/app.js
@@ -2,21 +2,17 @@
 
   var VARIANTS = [
       {
-          id: 'Opinion-UK-Control',
+          id: 'Opinion-UK-Legacy',
           test: function () {
-              const emailListID = '9999';
-              const exampleUrl = '';
+              const emailListID = '3814';
               updateWebView(emailListID);
-              updateExampleUrl(exampleUrl);
           }
       },
       {
-          id: 'Opinion-UK-Variant',
+          id: 'Opinion-UK-Connected',
           test: function () {
-              const emailListID = '8888';
-              const exampleUrl = '';
+              const emailListID = '3811';
               updateWebView(emailListID);
-              updateExampleUrl(exampleUrl);
           }
       }
   ];
@@ -24,28 +20,23 @@
   function isWebView() {
     // window.GU (case is important!) only exists in the app.
     // Therefore, window.GU can be used to detect when an article is running in an App WebView
-    return (typeof window.GU !== 'undefined') ? true : false;
+    return (typeof window.GU !== 'undefined');
   }
 
   function getRandomBucket(variants) {
-    return variants[Math.floor(
-      Math.random() * variants.length
-    )];
+    if (Math.random() < 0.1) {
+        return variants[0];
+    } else {
+        return variants[1];
+    }
   }
 
   function updateWebView(emailListID) {
       const emailForm = document.getElementsByClassName('js-email-sub__iframe')[0];
-      const formUrl = 'https://www.theguardian.com/email/form/plaintone/' + emailListID
+      const formUrl = 'https://www.theguardian.com/email/form/plaintone/' + emailListID;
       if (emailForm) {
         emailForm.setAttribute('src', formUrl);
       }
-  }
-
-  function updateExampleUrl(exampleUrl) {
-    const example = document.getElementsByClassName('js-email-example')[0];
-    if(example) {
-      example.setAttribute('href', exampleUrl);
-    }
   }
 
   function init(variants) {
