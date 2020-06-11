e7c4512ecf9133de7fad3c97b4c6b89e7e659d47
superfrank <frank.hulley-jones@guardian.co.uk>
2020-06-11 17:49:39 +0100

    update to latest template

--- bookmarks/gulpfile.babel.js
+++ bookmarks/gulpfile.babel.js
@@ -1,36 +1,36 @@
-import config from './config.json'
-
-import gulp from 'gulp'
-import AWS from 'aws-sdk'
-import file from 'gulp-file'
-import gutil from 'gulp-util'
-import s3 from 'gulp-s3-upload';
-import sass from 'gulp-sass'
-import size from 'gulp-size'
-import sourcemaps from 'gulp-sourcemaps'
-import template from 'gulp-template'
-
-import browserSync from 'browser-sync'
-import buffer from 'vinyl-buffer'
-import del from 'del'
-import fs from 'fs'
-import inquirer from 'inquirer'
-import rollup from 'rollup-stream'
-import rp from 'request-promise-native'
-import runSequence from 'run-sequence'
-import source from 'vinyl-source-stream'
-
+const { series, dest, src, parallel, watch } = require("gulp");
+const del = require("del");
+const gutil = require("gulp-util");
+const rename = require("gulp-rename");
+const requireUncached = require("require-uncached");
+const s3 = require("gulp-s3-upload");
+const fs = require("fs");
+const template = require('gulp-template');
+const replace = require('gulp-replace');
+const sass = require("gulp-sass");
+const file = require("gulp-file");
+sass.compiler = require("node-sass");
+const browserSync = require("browser-sync");3
 const browser = browserSync.create();
-
-const buildDir = '.build';
+const cleanCSS = require('gulp-clean-css');
+const es = require('event-stream');
+const mergeStream = require('merge-stream');
+const config = require("./config.json")
+const path = require("path")
+const named = require("vinyl-named")
 const cdnUrl = 'https://interactive.guim.co.uk';
+const webpack = require('webpack')
+const ws = require('webpack-stream')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const mkdirp = require("mkdirp")
+const rp = require("request-promise")
 
-const isDeploy = gutil.env._.indexOf('deploy') > -1;
+const isDeploy = gutil.env._.indexOf('deploylive') > -1 || gutil.env._.indexOf('deploypreview') > -1
+const live = gutil.env._.indexOf('deploylive') > -1
 
 const version = `v/${Date.now()}`;
 const s3Path = `atoms/${config.path}`;
-const s3VersionPath = `${s3Path}/${version}`;
-const path = isDeploy ? `${cdnUrl}/${s3VersionPath}` : '.';
+const assetPath = isDeploy ? `${cdnUrl}/${s3Path}/assets/${version}` : '../assets';
 
 // hack to use .babelrc environments without env var, would be nice to
 // be able to pass "client" env through to babel
@@ -38,188 +38,239 @@ const babelrc = JSON.parse(fs.readFileSync('.babelrc'));
 const presets = (babelrc.presets || []).concat(babelrc.env.client.presets);
 const plugins = (babelrc.plugins || []).concat(babelrc.env.client.plugins);
 
-const rollupPlugins = [
-    require('rollup-plugin-json')(),
-    require('rollup-plugin-string')({
-        'include': '**/*.html'
-    }),
-    require('rollup-plugin-node-resolve')({
-        'jsnext': true, 'browser': true
-    }),
-    require('rollup-plugin-commonjs')({
-        'include': ['node_modules/**']
-    }),
-    require('rollup-plugin-babel')({
-        'exclude': 'node_modules/**',
-        'babelrc': false,
-        presets, plugins
-    }),
-    isDeploy && require('rollup-plugin-uglify')()
+
+let webpackPlugins = [
+  new webpack.LoaderOptionsPlugin({
+      options: {
+          babel: {
+              presets,
+              plugins
+          }
+      }
+  })
 ];
 
-function logError(plugin, err) {
-    console.error(new gutil.PluginError(plugin, err.message).toString());
-    if (err instanceof SyntaxError) {
-        console.error(err.codeFrame);
-    }
+const clean = () => {
+  return del([".build"]);
 }
 
-function buildJS(filename) {
-    return () => {
-        return rollup({
-                'entry': `./src/js/${filename}`,
-                'sourceMap': true,
-                'plugins': rollupPlugins,
-                'format': 'iife'
-            })
-            .on('error', function (err) {
-                logError('rollup', err);
-                this.emit('end');
-            })
-            .pipe(source(filename, './src/js'))
-            .pipe(buffer())
-            .pipe(template({path}))
-            .pipe(sourcemaps.init({'loadMaps': true}))
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest(buildDir));
-    }
-}
-
-function s3Upload(cacheControl, keyPrefix) {
-    return s3()({
-        'Bucket': 'gdn-cdn',
-        'ACL': 'public-read',
-        'CacheControl': cacheControl,
-        'keyTransform': fn => `${keyPrefix}/${fn}`
-      }, {
-        'credentials': new AWS.SharedIniFileCredentials({profile: 'interactives'})
-    });
-}
-
-function requireUncached(module) {
-    delete require.cache[require.resolve(module)];
-    return require(module);
-}
-
-function readOpt(fn) {
-    try {
-        return fs.readFileSync(fn);
-    } catch (err) {
-        return '';
-    }
-}
-
-gulp.task('clean', () => del(buildDir));
-
-gulp.task('build:css', () => {
-    return gulp.src('src/css/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            'outputStyle': isDeploy ? 'compressed' : 'expanded'
-        }).on('error', sass.logError))
-        .pipe(sourcemaps.write('.'))
-        .pipe(template({path}))
-        .pipe(gulp.dest(buildDir))
-        .pipe(browser.stream({'match': '**/*.css'}));
-});
-
-gulp.task('build:js.main', buildJS('main.js'));
-gulp.task('build:js.app', buildJS('app.js'));
-gulp.task('build:js', ['build:js.main', 'build:js.app']);
-
-gulp.task('build:html', cb => {
-    try {
-        let render = requireUncached('./src/render.js').render;
-
-        Promise.resolve(render()).then(html => {
-            file('main.html', html, {'src': true})
-                .pipe(template({path}))
-                .pipe(gulp.dest(buildDir))
-                .on('end', cb);
-        }).catch(err => {
-            logError('render.js', err);
-            cb();
-        });
-    } catch (err) {
-        logError('render.js', err);
-        cb();
-    }
-});
-
-gulp.task('build:assets', () => {
-    return gulp.src('src/assets/**/*').pipe(gulp.dest(`${buildDir}/assets`));
-});
-
-gulp.task('_build', ['clean'], cb => {
-    runSequence(['build:css', 'build:js', 'build:html', 'build:assets'], cb);
-});
-
-// TODO: less hacky build/_build?
-gulp.task('build', ['_build'], () => {
-    return gulp.src(`${buildDir}/**/!(*.map)`)
-        .pipe(size({'gzip': true, 'showFiles': true}))
-});
-
-gulp.task('deploy', ['build'], cb => {
-    inquirer.prompt({
-        type: 'list',
-        name: 'env',
-        message: 'Where would you like to deploy to?',
-        choices: ['preview', 'live']
-    }).then(res => {
-        let isLive = res.env === 'live';
-        gulp.src(`${buildDir}/**/*`)
-            .pipe(s3Upload('max-age=31536000', s3VersionPath))
-            .on('end', () => {
-                gulp.src('config.json')
-                    .pipe(file('preview', version))
-                    .pipe(isLive ? file('live', version) : gutil.noop())
-                    .pipe(s3Upload('max-age=30', s3Path))
-                    .on('end', cb);
-            });
-    });
-});
-
-gulp.task('local', ['build'], () => {
-    return gulp.src('harness/*')
-        .pipe(template({
-            'css': readOpt(`${buildDir}/main.css`),
-            'html': readOpt(`${buildDir}/main.html`),
-            'js': readOpt(`${buildDir}/main.js`)
-        }))
-        .pipe(gulp.dest(buildDir));
-});
-
-gulp.task('default', ['local'], () => {
-    gulp.watch('src/**/*', ['local']).on('change', evt => {
-        console.log();
-        gutil.log(gutil.colors.yellow(`${evt.path} was ${evt.type}`));
+const render = async (cb) => {
+  try {
+    const atoms = (fs.readdirSync("atoms")).filter(n => n.slice(0, 1) !== ".");
+    const renders = atoms.map(atom => {
+      const render = requireUncached(`./atoms/${atom}/server/render.js`).render;
+      return Promise.resolve(render());
     });
 
-    browser.init({
-        'server': {'baseDir': buildDir},
-        'port': 8000
-    });
-});
+    const htmls = await Promise.all(renders)
 
-gulp.task('url', () => {
-    gutil.log(gutil.colors.green(`Atom URL: https://content.guardianapis.com/atom/interactive/interactives/${config.path}`));
-});
+    htmls.forEach((html, i) => {
+      const atom = atoms[i];
+      mkdirp.sync(`.build/${atom}`)
+      fs.writeFileSync(`.build/${atom}/main.html`, html);
+    })
+  } catch (err) {
+    console.log(err);
+  }
+  cb();
+}
 
-gulp.task('log', () => {
-    function log(type) {
-        let url = `${cdnUrl}/atoms/${config.path}/${type}.log?${Date.now()}`;
-        return rp(url).then(log => {
-            gutil.log(gutil.colors.green(`Got ${type} log:`));
-            console.log(log);
-        }).catch(err => {
-            if (err.statusCode === 404) {
-                gutil.log(gutil.colors.yellow(`No ${type} log, have you ever deployed?`));
-            } else {
-                throw err;
-            }
-        });
+const buildJS = () => {
+    return src("atoms/**/client/js/*.js")
+      .pipe(named((file) => file.relative.replace(/.js/g, "")))
+      .pipe(ws({
+        watch: false,
+        mode: isDeploy ? 'production' : 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    loader: 'style!css'
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.jsx$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.html$/,
+                    use: 'raw-loader'
+                }
+            ]
+        },
+        devtool: 'source-map',
+        optimization : { minimizer: [new UglifyJsPlugin()] },
+        plugins: webpackPlugins,
+        resolve: {
+          alias: {
+            "shared": path.resolve(__dirname, 'shared'),
+            "data": path.resolve(__dirname, '../data')
+          }
+        }
+    }, webpack))
+    .on('error', function handleError(e) {
+      this.emit('end'); // Recover from errors
+    })
+    .pipe(rename((path) => {
+      path.dirname = path.dirname.replace(/client/g, "");
+    })) 
+    .pipe(replace('<%= path %>', assetPath))
+    .pipe(dest(".build/"));
+}
+
+const buildCSS = () => {
+  return src("atoms/**/client/css/*.scss")
+  .pipe(replace('<%= path %>', path))
+    .pipe(sass({
+      includePaths: [
+        path.resolve(__dirname, 'shared/css')
+      ]
+    }).on("error", sass.logError))
+    .pipe(rename((path) => {
+      path.dirname = path.dirname.replace(/client\/css/g, "");
+    }))
+    .pipe(template({
+      path: assetPath,
+      atomPath : `<%= atomPath %>`
+    }))
+    .pipe(isDeploy ? cleanCSS({compatibility: 'ie8'}) : gutil.noop())
+    .pipe(dest(".build"))
+    .pipe(browser.stream({
+      'match': '**/*.css'
+    }));
+};
+
+const assets = () => {
+  return src("assets/**/*")
+    .pipe(dest(".build/assets/"))
+}
+
+const _template = (x) => {
+  return x
+    .replace(/<%= path %>/g, assetPath)
+    .replace(/&lt;%= path %&gt;/g, assetPath)
+    .replace(/<%= atomPath %>/g, `.`)
+}
+
+const local = () => {
+  const atoms = getAtoms();
+
+  const atomPromises = atoms.map(atom => { 
+    const js = _template((fs.readFileSync(`.build/${atom}/main.js`)).toString());
+    const css = _template((fs.readFileSync(`.build/${atom}/main.css`)).toString());
+    const html = _template((fs.readFileSync(`.build/${atom}/main.html`)).toString());
+    
+    return src(["harness/*", "!harness/_index.html"])
+      .pipe(template({js,css,html,atom,version}))
+      .pipe(dest(".build/" + atom))
+  });
+
+  atomPromises.push(src("harness/_index.html")
+    .pipe(template({
+      atoms
+    }))
+    .pipe(rename((path) => {
+      path.basename = "index";
+    }))
+    .pipe(dest(".build")))
+
+  return mergeStream(atomPromises)
+}
+
+const serve = () => {
+  browser.init({
+      'server': {
+          'baseDir': ".build"
+      },
+      'port': 8000
+  });
+
+  watch(["atoms/**/*", "shared/**/*", "!**/*.scss"], series(build, local));
+  watch(["atoms/**/*.scss","shared/**/*.scss"], series(buildCSS, local))
+}
+
+const s3Upload = (cacheControl, keyPrefix) => {
+  return s3()({
+      'Bucket': 'gdn-cdn',
+      'ACL': 'public-read',
+      'CacheControl': cacheControl,
+      'keyTransform': fn => `${keyPrefix}/${fn}`
+  });
+}
+
+const upload = () => {
+  const atoms = (fs.readdirSync(".build")).filter(n => n !== "assets");
+
+  const uploadTasks = atoms.map(atom => {
+    const atomConfig = {
+      "title": `${config.title} – ${atom}`,
+      "docData": "",
+      "path": `${config.path}/${atom}`
     }
 
-    return Promise.all([log('live'), log('preview')]);
-});
+    return src(`.build/${atom}/*`)
+    .pipe(replace('<%= path %>', assetPath))
+    .pipe(replace('&lt;%= path %&gt;', assetPath))
+    .pipe(replace('<%= atomPath %>', `${cdnUrl}/${s3Path}/${atom}/${version}`))
+        .pipe(s3Upload('max-age=31536000', `${s3Path}/${atom}/${version}`))
+        .on("end", () => {
+          return file('config.json', JSON.stringify(atomConfig))
+            .pipe(file('preview', version))
+            .pipe(live ? file('live', version) : gutil.noop())
+            .pipe(s3Upload('max-age=30', `${s3Path}/${atom}`))
+        })
+  });
+
+  uploadTasks.push(
+    src(`.build/assets/**/*`)
+        .pipe(s3Upload('max-age=31536000', `${s3Path}/assets/${version}`))
+  );
+  
+  return mergeStream(uploadTasks)
+}
+
+const getAtoms = () => (fs.readdirSync(".build")).filter(n => n !== "assets" && n !== "index.html")
+
+const url = (cb) => {
+  const atoms = getAtoms();
+
+  atoms.forEach(atom => {
+    gutil.log(gutil.colors.yellow(`${atom} url:`));
+    gutil.log(gutil.colors.yellow(`https://content.guardianapis.com/atom/interactive/interactives/${config.path}/${atom}`));
+  });
+
+  cb();
+}
+
+const getLogs = async(cb) => {
+  const atoms = getAtoms();
+
+  for(let i = 0; i < atoms.length; i++) {
+    const atom = atoms[i];
+    const liveLog = await rp(`${cdnUrl}/atoms/${config.path}/${atom}/live.log`).catch(err => console.log(`LIVE REQUEST ERROR: ${atom}`));
+    const previewLog = await rp(`${cdnUrl}/atoms/${config.path}/${atom}/preview.log`).catch(err => console.log(`PREVIEW REQUEST ERROR: ${atom}`));
+
+    console.log(`${atom} live log:`)
+    console.log(liveLog)
+    console.log(`${atom} preview log:`)
+    console.log(previewLog)
+  }
+
+  cb();
+}
+
+const build = series(clean, parallel(buildJS, buildCSS, render, assets));
+const deploy = series(build, upload)
+
+
+exports.build = build;
+exports.deploylive = deploy;
+exports.deploypreview = deploy;
+exports.log = getLogs;
+exports.url = url;
+exports.default = series(build, local, serve);
--- bookmarks/src/render.js
+++
@@ -1,5 +0,0 @@
-import mainTemplate from './src/templates/main.html!text'
-
-export async function render() {
-    return mainTemplate;
-}
